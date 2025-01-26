eff7f7889822b33204501b4a90c5c2f734604376
Oscar Racero - Techdevai.es <159945416+techdevai-es@users.noreply.github.com>
2025-01-26 20:25:26 +0100

    Update rss.xml.ts
    actualizado campos

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,21 +7,23 @@ type Context = {
 }
 
 export async function GET(context: Context) {
-	const posts = await getCollection("blog")
+  const posts = await getCollection("blog")
   const projects = await getCollection("projects")
 
   const items = [...posts, ...projects]
 
-  items.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+  // Cambié "date" a "fecha" para los nuevos nombres de campos.
+  items.sort((a, b) => new Date(b.data.fecha).getTime() - new Date(a.data.fecha).getTime())
 
   return rss({
     title: SITE.TITLE,
     description: SITE.DESCRIPTION,
     site: context.site,
     items: items.map((item) => ({
-      title: item.data.title,
-      description: item.data.summary,
-      pubDate: item.data.date,
+      // Cambié "title" a "titulo", "summary" a "resumen", y "date" a "fecha".
+      title: item.data.titulo,
+      description: item.data.resumen,
+      pubDate: item.data.fecha,
       link: item.slug.startsWith("blog")
         ? `/blog/${item.slug}/`
         : `/projects/${item.slug}/`,
